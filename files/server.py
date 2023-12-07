b8153a67349cdcd3a921cafb75c497e385f18f7a
= <snytkinaleksandr2@gmail.com>
2023-12-07 19:17:01 +0700

    3Net

--- files/server.py
+++ files/server.py
@@ -18,9 +18,8 @@ app = Flask(__name__)
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 # file_name_result = os.getenv('file_name_result')
-num_token = int(os.getenv('num_token'))
-src_result = os.getenv('src_result')
-
+num_token = int(os.getenv('NUM_TOKEN'))
+src_result = os.getenv('SRC_RESULT')
 
 classes = {0: 'drone', 1: 'noise', 2: 'wifi'}
 token = 1
@@ -80,15 +79,17 @@ def receive_data():
     global model_1
     global model_2
     global token
-    #global result
-    #global result_dict
+    # global result
+    # global result_dict
 
     data = json.loads(request.json)
     print()
     print('Получен пакет ' + str(token))
 
     freq = int(data['freq'])
-    img = np.asarray(sig2pic(np.asarray(data['data_real'], dtype=np.float32), np.asarray(data['data_imag'], dtype=np.float32)), dtype=np.float32)
+    img = np.asarray(
+        sig2pic(np.asarray(data['data_real'], dtype=np.float32), np.asarray(data['data_imag'], dtype=np.float32)),
+        dtype=np.float32)
     img_torch = torch.unsqueeze(torch.tensor(img), 0).to(device)
     with torch.no_grad():
         output_1 = model_1(img_torch)
@@ -106,7 +107,6 @@ def receive_data():
     probability = softmax(output_1)[label]
     print('Уверенность модели 1 в предсказании: ' + str(round(probability, 3)))
     print('Загрузка картинок ...')
-    np.save(str(src_result) + 'result_' + str(token) + '_' + str(classes[int(prediction_1)]) + '.npy', img)
     fig, ax = plt.subplots()
     ax.imshow(img[0], cmap='gray')
     plt.savefig(src_result + 'token_' + str(token) + '_' + str(classes[int(prediction_1)]) + '_real_1.png')
@@ -152,19 +152,21 @@ def receive_data():
     plt.close()
     print('Загрузка картинок завершена')
 
+    np.save(str(src_result) + 'result_' + str(token) + '_' + str(classes[int(prediction_1)]) + '_' + str(classes[int(prediction_2)]) + '_' + str(classes[int(prediction_3)]) + '.npy', img)
     token += 1
     print('Инференс завершён')
     print()
 
-    if token-1 == num_token:
+    if token - 1 == num_token:
         print('Завершение работы')
         # print(result_dict)
         # with open(file_name_result, 'w') as f:
-            # f.write(result)
+        # f.write(result)
         # print('Файл с результатами записан!')
         sys.exit()
 
-    result_msg = {'message': 'Data inference successfully', 'prediction_1': classes[int(prediction_1)], 'prediction_2': classes[int(prediction_2)], 'prediction_3': classes[int(prediction_3)]}
+    result_msg = {'message': 'Data inference successfully', 'prediction_1': classes[int(prediction_1)],
+                  'prediction_2': classes[int(prediction_2)], 'prediction_3': classes[int(prediction_3)]}
     return jsonify(result_msg)
 
 
@@ -174,15 +176,17 @@ def load_function(attr):
 
 
 def build_model():
-    path_to_nn_1 = os.getenv('path_to_nn_1')
-    path_to_nn_2 = os.getenv('path_to_nn_2')
-    path_to_nn_3 = os.getenv('path_to_nn_3')
-    src_ml_config_18 = os.getenv('src_ml_config_18')
-    src_ml_config_50 = os.getenv('src_ml_config_50')
+    path_to_nn_1 = os.getenv('PATH_TO_NN_1')
+    path_to_nn_2 = os.getenv('PATH_TO_NN_2')
+    path_to_nn_3 = os.getenv('PATH_TO_NN_3')
+    src_ml_config_18 = os.getenv('SRC_ML_CONFIG_18')
+    src_ml_config_50 = os.getenv('SRC_ML_CONFIG_50')
+    print(src_ml_config_50)
 
     config_18 = mlconfig.load(src_ml_config_18)
     model_1 = load_function(config_18.model.architecture)(pretrained=False)
-    lin = torch.nn.Sequential(torch.nn.Conv2d(2, 3, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False), model_1.conv1)
+    lin = torch.nn.Sequential(torch.nn.Conv2d(2, 3, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
+                              model_1.conv1)
     model_1.conv1 = lin
     model_1.fc = torch.nn.Linear(in_features=512, out_features=3, bias=True)
 
@@ -195,7 +199,8 @@ def build_model():
     print('Модель 1 загружена')
 
     model_2 = load_function(config_18.model.architecture)(pretrained=False)
-    lin = torch.nn.Sequential(torch.nn.Conv2d(2, 3, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False), model_2.conv1)
+    lin = torch.nn.Sequential(torch.nn.Conv2d(2, 3, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
+                              model_2.conv1)
     model_2.conv1 = lin
     model_2.fc = torch.nn.Linear(in_features=512, out_features=3, bias=True)
 
@@ -208,7 +213,8 @@ def build_model():
 
     config_50 = mlconfig.load(src_ml_config_50)
     model_3 = load_function(config_50.model.architecture)(pretrained=False)
-    lin = torch.nn.Sequential(torch.nn.Conv2d(2, 3, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False), model_3.conv1)
+    lin = torch.nn.Sequential(torch.nn.Conv2d(2, 3, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
+                              model_3.conv1)
     model_3.conv1 = lin
     model_3.fc = torch.nn.Sequential(
         torch.nn.Linear(in_features=2048, out_features=512, bias=True),
@@ -229,8 +235,8 @@ def build_model():
 
 def run_example():
     print('Пробный запуск модели')
-    src_example = os.getenv('src_example')
-
+    src_example = os.getenv('SRC_EXAMPLE')
+    print(src_example)
     img = torch.unsqueeze(torch.tensor(np.asarray(np.load(src_example, 'r+'), dtype=np.float32)), 0).to(device)
     with torch.no_grad():
         output_1 = model_1(img)
@@ -259,13 +265,46 @@ def run_example():
     print('Уверенность модели 3 в пробном предсказании: ' + str(round(probability, 3)))
     print('Тестовый инференс завершён')
     print()
+    for file in os.listdir(src_example):
+        img = torch.unsqueeze(torch.tensor(np.asarray(np.load(src_example, 'r+'), dtype=np.float32)), 0).to(device)
+        with torch.no_grad():
+            output_1 = model_1(img)
+            output_2 = model_2(img)
+            output_3 = model_3(img)
+            _, predict_1 = torch.max(output_1.data, 1)
+            _, predict_2 = torch.max(output_2.data, 1)
+            _, predict_3 = torch.max(output_3.data, 1)
+        prediction = np.asarray(predict_1.cpu())[0]
+        print('Пробное предсказание 1: ' + str(classes[int(prediction)]))
+        label = np.asarray(np.argmax(output_1, axis=1))[0]
+        output_1 = np.asarray(torch.squeeze(output_1, 0))
+        probability = softmax(output_1)[label]
+        print('Уверенность модели 1 в пробном предсказании: ' + str(round(probability, 3)))
+        prediction = np.asarray(predict_2.cpu())[0]
+        print('Пробное предсказание 2: ' + str(classes[int(prediction)]))
+        label = np.asarray(np.argmax(output_2, axis=1))[0]
+        output_2 = np.asarray(torch.squeeze(output_2, 0))
+        probability = softmax(output_2)[label]
+        print('Уверенность модели 2 в пробном предсказании: ' + str(round(probability, 3)))
+        prediction = np.asarray(predict_3.cpu())[0]
+        print('Пробное предсказание 3: ' + str(classes[int(prediction)]))
+        label = np.asarray(np.argmax(output_3, axis=1))[0]
+        output_3 = np.asarray(torch.squeeze(output_3, 0))
+        probability = softmax(output_3)[label]
+        print('Уверенность модели 3 в пробном предсказании: ' + str(round(probability, 3)))
+        print('Тестовый инференс завершён')
+        print()
 
 
 if __name__ == '__main__':
 
-    model_1, model_2,model_3 = build_model()
-    run_example()
+    model_1, model_2, model_3 = build_model()
+    try:
+        run_example()
+    except Exception as e:
+        print(e.__str__())
 
-    server_ip = os.getenv('server_ip')
-    server_port = os.getenv('server_port')
+    server_ip = os.getenv('SERVER_IP')
+    server_port = os.getenv('SERVER_PORT')
+    print(server_ip, server_port)
     app.run(host=server_ip, port=server_port)
